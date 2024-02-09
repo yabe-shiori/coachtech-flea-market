4a264bd712a67c1f01c332c62a01e284c569873b
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-02-09 20:15:28 +0900

    Edited login and registration screens

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,10 +5,13 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
+        <h2 class="text-2xl text-center font-bold mt-8 mb-10">{{ __('ログイン') }}</h2>
+
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -16,32 +19,37 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
-        <div class="block mt-4">
+        {{-- <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
-        </div>
+        </div> --}}
 
-        <div class="flex items-center justify-end mt-4">
+        {{-- <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
+            @endif --}}
+        <div class="mt-14 w-full">
+            <x-primary-button class="w-full bg-red-400 font-bold flex items-center justify-center">
+                ログインする
             </x-primary-button>
+        </div>
+
+        {{-- </div> --}}
+        <div class="text-center mt-4">
+            <a class="text-blue-500 hover:underline text-sm hover:text-blue-700" href="{{ route('register') }}">
+                会員登録はこちら
+            </a>
         </div>
     </form>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,12 +2,14 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
+        <h2 class="text-2xl text-center font-bold mt-8 mb-10">{{ __('会員登録') }}</h2>
+
         <!-- Name -->
-        <div>
+        {{-- <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+        </div> --}}
 
         <!-- Email Address -->
         <div class="mt-4">
@@ -29,7 +31,7 @@
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
+        {{-- <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
@@ -37,16 +39,22 @@
                             name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+        </div> --}}
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+        <div class="flex items-center justify-end mt-14">
+            {{-- <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
-            </a>
+            </a> --}}
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
+            <x-primary-button class="w-full bg-red-400 font-bold flex items-center justify-center">
+                登録する
             </x-primary-button>
         </div>
+
+        <div class="text-center mt-4">
+                <a class="text-blue-500 hover:underline text-base hover:text-blue-700" href="{{ route('login') }}">
+                    ログインはこちら
+                </a>
+            </div>
     </form>
 </x-guest-layout>
