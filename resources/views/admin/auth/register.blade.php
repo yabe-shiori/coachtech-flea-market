7e8255a36c40f32570ed5b63e17ef996a646d37c
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-02-10 17:16:18 +0900

    Added functionality for user registration with email and password only

--- resources/views/admin/auth/register.blade.php
+++ resources/views/admin/auth/register.blade.php
@@ -6,11 +6,11 @@
         <h2 class="text-2xl text-center font-bold mt-8 mb-10">{{ __('会員登録') }}</h2>
 
         <!-- Name -->
-        {{-- <div>
+        <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div> --}}
+        </div>
 
         <!-- Email Address -->
         <div class="mt-4">
@@ -32,7 +32,7 @@
         </div>
 
         <!-- Confirm Password -->
-        {{-- <div class="mt-4">
+        <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
@@ -40,7 +40,7 @@
                             name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div> --}}
+        </div>
 
         <div class="flex items-center justify-end mt-14">
             {{-- <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('admin.login') }}">
