be83ddfe87291937e20f5dc1ce6a36135ed0dc4a
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-02-10 19:57:08 +0900

    Adjusted layout in user-navigation.blade.php

--- resources/views/layouts/user-navigation.blade.php
+++ resources/views/layouts/user-navigation.blade.php
@@ -1,34 +1,35 @@
 <nav x-data="{ open: false }" class="bg-black border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="bg-black shrink-0 flex items-center">
-                    <a href="{{ route('user.dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-white" />
-                    </a>
-                </div>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between h-16">
+    <div class="flex">
+        <!-- Logo -->
+        <div class="bg-black shrink-0 flex items-center">
+            <a href="{{ route('user.dashboard') }}">
+                <x-application-logo class="block h-9 w-auto fill-current text-white" />
+            </a>
+        </div>
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <form id="logoutForm" method="POST" action="{{ route('user.logout') }}">
-                        @csrf
-                    </form>
-                    <x-nav-link href="#"
-                        onclick="event.preventDefault(); document.getElementById('logoutForm').submit();"
-                        class="text-white">
-                        ログアウト
-                    </x-nav-link>
+        <!-- Navigation Links -->
+        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex items-center">
+            <form id="logoutForm" method="POST" action="{{ route('user.logout') }}">
+                @csrf
+            </form>
+            <x-nav-link href="#"
+                onclick="event.preventDefault(); document.getElementById('logoutForm').submit();"
+                class="text-white">
+                ログアウト
+            </x-nav-link>
 
-                    <x-nav-link :href="route('user.dashboard')" :active="request()->routeIs('user.dashboard')" class="text-white">
-                        マイページ
-                    </x-nav-link>
-                    <x-nav-link :href="route('user.dashboard')" :active="request()->routeIs('user.dashboard')" class="text-white">
-                        出品
-                    </x-nav-link>
-                </div>
-            </div>
+            <x-nav-link :href="route('user.dashboard')" :active="request()->routeIs('user.dashboard')" class="text-white">
+                マイページ
+            </x-nav-link>
+            <x-nav-link :href="route('user.dashboard')" :active="request()->routeIs('user.dashboard')"
+                class="inline-flex items-center justify-center bg-white text-black px-4 py-1.5 rounded-md"
+                style="line-height: 1.25rem;">
+                出品
+            </x-nav-link>
+        </div>
+    </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
