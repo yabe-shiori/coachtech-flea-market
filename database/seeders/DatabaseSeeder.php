7b688677f1641e956eb8cf395ad0fe6bd4fe96dc
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-02-11 14:17:20 +0900

    Created ItemImagesTableSeeder and added initial data to the item_images table.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Item;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,6 +19,7 @@ class DatabaseSeeder extends Seeder
             CategoriesTableSeeder::class,
             BrandsTableSeeder::class,
             ItemsTableSeeder::class,
+            ItemImagesTableSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
--- database/seeders/ItemImagesTableSeeder.php
+++ database/seeders/ItemImagesTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class ItemImagesTableSeeder extends Seeder
 {
@@ -12,6 +14,80 @@ class ItemImagesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $itemImages = [
+            [
+                'item_id' => 1,
+                'image_path' => '/images/dummy1.png',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+
+            [
+                'item_id' => 2,
+                'image_path' => '/images/dummy2.png',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+
+            [
+                'item_id' => 3,
+                'image_path' => '/images/dummy3.png',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+
+            [
+                'item_id' => 4,
+                'image_path' => '/images/dummy4.png',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+
+            [
+                'item_id' => 5,
+                'image_path' => '/images/dummy5.png',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+
+            [
+                'item_id' => 6,
+                'image_path' => '/images/dummy6.png',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+
+            [
+                'item_id' => 7,
+                'image_path' => '/images/dummy7.png',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+
+            [
+                'item_id' => 8,
+                'image_path' => '/images/dummy8.png',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+
+            [
+                'item_id' => 9,
+                'image_path' => '/images/dummy9.png',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            
+            [
+                'item_id' => 10,
+                'image_path' => '/images/dummy10.png',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ];
+
+        foreach ($itemImages as $itemImage) {
+            DB::table('item_images')->insert($itemImage);
+        }
     }
 }
