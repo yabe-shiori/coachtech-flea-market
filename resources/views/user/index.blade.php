218e01af1fc0f24cbf21343d95de3e1c7b5e118d
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-02-11 15:38:18 +0900

    Modified user index page

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -7,13 +7,14 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-             <h1 class="text-black">商品一覧</h1>
+            <h1 class="text-black">商品一覧</h1>
             <div class="grid grid-cols-5 gap-4">
                 @foreach($itemImages as $item)
                     @foreach($item->images as $image)
-                        <div>
+                        <div class="relative">
                             <a href="#">
                                 <img src="{{ $image->image_path }}" alt="{{ $item->name }}" class="w-full">
+                                <span class="absolute bottom-0 left-0 px-2 py-1 bg-black bg-opacity-40 text-white rounded-tr-xl rounded-br-xl">¥{{ number_format($item->price) }}</span>
                             </a>
                         </div>
                     @endforeach
