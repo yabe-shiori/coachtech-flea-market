e8e55f23ff3a36724d3fcd817b4e281f50e7fbb1
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-02-13 14:40:14 +0900

    Updated register.blade.php

--- resources/views/user/auth/register.blade.php
+++ resources/views/user/auth/register.blade.php
@@ -4,62 +4,34 @@
 
         <h2 class="text-2xl text-center font-bold mt-8 mb-10">{{ __('会員登録') }}</h2>
 
-        <!-- Name -->
-        {{-- <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div> --}}
-
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
-<!-- Avatar -->
-            <div class="mt-4">
-                <x-input-label for="avatar" :value="__('プロフィール画像（任意・1MBまで）')" />
 
-                <x-text-input id="avatar" class="block mt-1 w-full rounded-none" type="file" name="avatar" :value="old('avatar')" />
-            </div>
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Confirm Password -->
-        {{-- <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div> --}}
-
         <div class="flex items-center justify-end mt-14">
-            {{-- <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('user.login') }}">
-                {{ __('Already registered?') }}
-            </a> --}}
-
             <x-primary-button class="w-full bg-red-400 font-bold flex items-center justify-center">
                 登録する
             </x-primary-button>
         </div>
 
         <div class="text-center mt-4">
-                <a class="text-blue-500 hover:underline text-base hover:text-blue-700" href="{{ route('user.login') }}">
-                    ログインはこちら
-                </a>
-            </div>
+            <a class="text-blue-500 hover:underline text-base hover:text-blue-700" href="{{ route('user.login') }}">
+                ログインはこちら
+            </a>
+        </div>
     </form>
 </x-guest-layout>
