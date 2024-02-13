aa2160e0b9c7bb216d782d2b944d2f1a9a90c9e2
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-02-13 14:40:55 +0900

    Updated login.blade.php

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -26,27 +26,12 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
-        {{-- <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div> --}}
-
-        {{-- <div class="flex items-center justify-end mt-4">
-            @if (Route::has('user.password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('user.password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif --}}
         <div class="mt-14 w-full">
             <x-primary-button class="w-full bg-red-400 font-bold flex items-center justify-center">
                 ログインする
             </x-primary-button>
         </div>
 
-        {{-- </div> --}}
         <div class="text-center mt-4">
             <a class="text-blue-500 hover:underline text-sm hover:text-blue-700" href="{{ route('user.register') }}">
                 会員登録はこちら
