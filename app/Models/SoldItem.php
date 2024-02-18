82b19f588517131b25d36e0a01940a1875a51cd4
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-02-18 14:39:03 +0900

    Created sold_items table and SoldItem model

---
+++ app/Models/SoldItem.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SoldItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'item_id',
+        'buyer_id',
+        'seller_id',
+        'sold_at',
+    ];
+
+    protected $dates = [
+        'sold_at'
+    ];
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
+
+    public function buyer()
+    {
+        return $this->belongsTo(User::class, 'buyer_id');
+    }
+
+    public function seller()
+    {
+        return $this->belongsTo(User::class, 'seller_id');
+    }
+
+
+}
---
+++ database/migrations/2024_02_18_142603_create_sold_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sold_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')->constrained();
+            $table->foreignId('buyer_id')->constrained('users');
+            $table->foreignId('seller_id')->constrained('users');
+            $table->timestamp('sold_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sold_items');
+    }
+};
