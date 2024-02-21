5f93528aaadaeffa2b08a250532f12da9f99ddaa
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-02-21 14:30:59 +0900

    Added display_name column to profiles table

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -11,7 +11,7 @@ class Profile extends Model
 
     protected $fillable = [
         'user_id',
-        'image',
+        'display_name',
         'introduction',
         'postal_code',
         'address',
