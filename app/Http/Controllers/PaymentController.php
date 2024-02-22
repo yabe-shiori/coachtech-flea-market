524dbcb48388d7b283ee1203de689d150773b55f
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-02-23 08:55:26 +0900

    Refactor PaymentController to prevent displaying payment page when item is sold out or user is not logged in

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,17 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Transaction;
-use App\Models\Payment;
+
 use App\Models\Item;
+use Illuminate\Support\Facades\Auth;
+
 
 class PaymentController extends Controller
 {
     // 支払いページの表示
     public function create($itemId)
     {
+        if (!Auth::check()) {
+            return back()->with('error', 'ログインしてください。');
+        }
+
         $item = Item::findOrFail($itemId);
+
+        if ($item->is_sold) {
+            return back()->with('error', '申し訳ありませんが、この商品は売り切れです。');
+        }
 
         return view('payment.checkout', compact('item'));
     }
