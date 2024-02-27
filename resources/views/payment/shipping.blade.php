d44343472d1c217af5c4713930caa7530bfbabb6
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-02-27 21:59:24 +0900

    Modified payments/shipping.blade.php

--- resources/views/payment/shipping.blade.php
+++ resources/views/payment/shipping.blade.php
@@ -2,11 +2,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <h2 class="text-2xl text-center font-bold mb-8">住所の変更</h2>
-            <form action="{{ route('user.profile.updateShippingAddress') }}" method="POST" class="max-w-lg mx-auto">
+            <form action="{{ route('user.profile.updateShippingAddress') }}" method="POST" class="max-w-xl mx-auto">
                 @method('PATCH')
                 @csrf
 
-                <div class="mb-6">
+                <div class="mb-8">
                     <label for="postal_code" class="block text-base font-medium text-gray-700">郵便番号</label>
                     <input type="text" name="postal_code" id="postal_code"
                         class="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
@@ -15,7 +15,7 @@
                     <x-validation-errors field="postal_code" />
                 </div>
 
-                <div class="mb-6">
+                <div class="mb-8">
                     <label for="address" class="block text-base font-medium text-gray-700">住所</label>
                     <textarea name="address" id="address" rows="3"
                         class="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
@@ -24,14 +24,14 @@
                 </div>
 
                 <div class="mb-10">
-                    <label for="building_name" class="block text-base font-medium text-gray-700">建物名（任意）</label>
+                    <label for="building_name" class="block text-base font-medium text-gray-700">建物名</label>
                     <input type="text" name="building_name" id="building_name"
                         class="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         placeholder="マンション名、部屋番号など"
                         value="{{ old('building_name', optional($user->profile)->building_name) }}">
                     <x-validation-errors field="building_name" />
                 </div>
-                <x-primary-button class="w-full">更新する</x-primary-button>
+                <x-primary-button class="w-full mt-8 py-2">更新する</x-primary-button>
             </form>
         </div>
     </div>
