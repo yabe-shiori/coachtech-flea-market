bdb85e9e4ea7208b78414ca4b4ae9b9c4c374cf9
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-02-27 21:34:33 +0900

    Updated layout of create.blade.php

--- resources/views/item/create.blade.php
+++ resources/views/item/create.blade.php
@@ -10,8 +10,7 @@
                     <form method="POST" action="{{ route('user.item.store') }}" enctype="multipart/form-data">
                         @csrf
                         <div class="mb-4 relative">
-                            <label for="image"
-                                class="block text-gray-700 text-sm font-bold mb-2 cursor-pointer">
+                            <label for="image" class="block text-gray-700 text-sm font-bold mb-2 cursor-pointer">
                                 <span
                                     class="block bg-white text-red-600 border-2 border-red-600 rounded-md py-2 px-4 w-full text-center">画像を選択する</span>
                             </label>
@@ -21,11 +20,10 @@
                         </div>
                         <div id="imagePreview" class="flex flex-wrap justify-start gap-4"></div>
 
-                        <h3 class="text-xl font-bold text-gray-500 border-b-2 border-gray-600 mb-4">商品の詳細</h3>
+                        <h3 class="text-xl font-bold border-b border-neutral-700 text-neutral-500 my-4">商品の詳細</h3>
 
                         <div class="mb-4">
-                            <label for="category"
-                                class="block text-gray-700 text-base font-bold mb-2">カテゴリー</label>
+                            <label for="category" class="block text-gray-700 text-base font-bold mb-2">カテゴリー</label>
                             <select name="category_id" id="category"
                                 class="border-2 border-gray-300 rounded-md p-2 w-full" required>
                                 <option value="">カテゴリーを選択してください</option>
@@ -52,8 +50,7 @@
                         </div>
 
                         <div class="mb-4">
-                            <label for="brand"
-                                class="block text-gray-700 text-base font-bold mb-2">ブランド（任意）</label>
+                            <label for="brand" class="block text-gray-700 text-base font-bold mb-2">ブランド（任意）</label>
                             <select name="brand_id" id="brand"
                                 class="border-2 border-gray-300 rounded-md p-2 w-full">
                                 <option value="">選択してください</option>
@@ -64,57 +61,48 @@
                         </div>
 
                         <div class="mb-4">
-                            <label for="condition"
-                                class="block text-black text-base font-bold mb-2">商品の状態</label>
+                            <label for="condition" class="block text-black text-base font-bold mb-2">商品の状態</label>
                             <select name="condition" id="condition"
                                 class="border-2 border-gray-300 rounded-md p-2 w-full" required>
                                 <option value="">状態を選択</option>
-                                <option value="新品、未使用"
-                                    {{ old('condition') == '新品、未使用' ? 'selected' : '' }}>新品、未使用
+                                <option value="新品、未使用" {{ old('condition') == '新品、未使用' ? 'selected' : '' }}>新品、未使用
                                 </option>
-                                <option value="未使用に近い"
-                                    {{ old('condition') == '未使用に近い' ? 'selected' : '' }}>未使用に近い
+                                <option value="未使用に近い" {{ old('condition') == '未使用に近い' ? 'selected' : '' }}>未使用に近い
                                 </option>
-                                <option value="目立った傷や汚れなし"
-                                    {{ old('condition') == '目立った傷や汚れなし' ? 'selected' : '' }}>
+                                <option value="目立った傷や汚れなし" {{ old('condition') == '目立った傷や汚れなし' ? 'selected' : '' }}>
                                     目立った傷や汚れなし</option>
-                                <option value="やや傷や汚れあり"
-                                    {{ old('condition') == 'やや傷や汚れあり' ? 'selected' : '' }}>
+                                <option value="やや傷や汚れあり" {{ old('condition') == 'やや傷や汚れあり' ? 'selected' : '' }}>
                                     やや傷や汚れあり</option>
-                                <option value="傷や汚れあり"
-                                    {{ old('condition') == '傷や汚れあり' ? 'selected' : '' }}>傷や汚れあり
+                                <option value="傷や汚れあり" {{ old('condition') == '傷や汚れあり' ? 'selected' : '' }}>傷や汚れあり
                                 </option>
-                                <option value="全体的に状態が悪い"
-                                    {{ old('condition') == '全体的に状態が悪い' ? 'selected' : '' }}>
+                                <option value="全体的に状態が悪い" {{ old('condition') == '全体的に状態が悪い' ? 'selected' : '' }}>
                                     全体的に状態が悪い</option>
                             </select>
                         </div>
 
-                        <h3 class="text-xl font-bold text-gray-500 border-b-2 border-gray-600 mb-4">商品名と説明</h3>
+                        <h3 class="text-xl font-bold border-b border-neutral-700 text-neutral-500 my-4">商品名と説明</h3>
 
                         <div class="mb-4">
                             <label for="name" class="block text-base font-bold mb-2">商品名</label>
                             <input type="text" name="name" id="name"
-                                class="border-2 border-gray-300 rounded-md p-2 w-full"
-                                value="{{ old('name') }}" required>
+                                class="border-2 border-gray-300 rounded-md p-2 w-full" value="{{ old('name') }}"
+                                required>
                             <x-validation-errors field="name" />
                         </div>
 
                         <div class="mb-4">
                             <label for="description" class="block text-base font-bold mb-2">商品の説明</label>
-                            <textarea name="description" id="description" rows="5"
-                                class="border-2 border-gray-300 rounded-md p-2 w-full"
+                            <textarea name="description" id="description" rows="5" class="border-2 border-gray-300 rounded-md p-2 w-full"
                                 required>{{ old('description') }}</textarea>
                         </div>
                         <x-validation-errors field="description" />
 
-                        <h3 class="text-xl font-bold text-gray-500 border-b-2 border-gray-600 mb-4">販売価格</h3>
+                        <h3 class="text-xl font-bold border-b border-neutral-700 text-neutral-500 my-4">販売価格</h3>
                         <div class="mb-4">
                             <label for="price" class="block text-base font-bold mb-2">販売価格</label>
                             <div class="flex items-center border-2 border-gray-300 rounded-md">
-                                <input type="number" name="price" id="price"
-                                    class="flex-1 p-2 rounded-r-md" placeholder="￥"
-                                    value="{{ old('price') }}" required>
+                                <input type="number" name="price" id="price" class="flex-1 p-2 rounded-r-md"
+                                    placeholder="￥" value="{{ old('price') }}" required>
                             </div>
                             <x-validation-errors field="price" />
                         </div>
