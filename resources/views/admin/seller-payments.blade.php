b689f071a2ac0237d08eda9c2f461e3394061426
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-02-28 19:32:56 +0900

    Modified admin/seller-payments.blade.php

--- resources/views/admin/seller-payments.blade.php
+++ resources/views/admin/seller-payments.blade.php
@@ -3,30 +3,35 @@
         <h1 class="text-xl font-bold mb-6">出品者への送金額確認</h1>
 
         <div class="overflow-x-auto">
-            <table class="min-w-full bg-white shadow-md rounded overflow-hidden">
-                <thead class="bg-gray-800 text-white">
-                    <tr>
-                        <th class="px-4 py-2 border border-black">No</th>
-                        <th class="px-4 py-2 border border-black">商品ID</th>
-                        <th class="px-4 py-2 border border-black">商品名</th>
-                        <th class="px-4 py-2 border border-black">出品者</th>
-                        <th class="px-4 py-2 border border-black">売却金額</th>
-                        <th class="px-4 py-2 border border-black">売却日時</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($soldItems as $key => $item)
-                        <tr class="{{ $key % 2 == 0 ? 'bg-gray-100' : '' }}">
-                            <td class="px-4 py-2 border border-black">{{ $soldItems->firstItem() + $key }}</td>
-                            <td class="px-4 py-2 border border-black">{{ $item->item->id }}</td>
-                            <td class="px-4 py-2 border border-black">{{ $item->item->name }}</td>
-                            <td class="px-4 py-2 border border-black">{{ $item->item->user->name }}</td>
-                            <td class="px-4 py-2 border border-black">{{ number_format($item->item->price, 0) }}</td>
-                            <td class="px-4 py-2 border border-black">{{ $item->sold_at }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="shadow-md rounded overflow-hidden">
+                <div class="min-w-full overflow-x-auto">
+                    <table class="w-full">
+                        <thead class="bg-gray-800 text-white">
+                            <tr>
+                                <th class="px-4 py-2 border border-black">No</th>
+                                <th class="px-4 py-2 border border-black">商品ID</th>
+                                <th class="px-4 py-2 border border-black">商品名</th>
+                                <th class="px-4 py-2 border border-black">出品者</th>
+                                <th class="px-4 py-2 border border-black">売却金額</th>
+                                <th class="px-4 py-2 border border-black">売却日時</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($soldItems as $key => $item)
+                                <tr class="{{ $key % 2 == 0 ? 'bg-gray-100' : '' }}">
+                                    <td class="px-4 py-2 border border-black">{{ $soldItems->firstItem() + $key }}</td>
+                                    <td class="px-4 py-2 border border-black">{{ $item->item->id }}</td>
+                                    <td class="px-4 py-2 border border-black">{{ $item->item->name }}</td>
+                                    <td class="px-4 py-2 border border-black">{{ $item->item->user->name }}</td>
+                                    <td class="px-4 py-2 border border-black">{{ number_format($item->item->price, 0) }}
+                                    </td>
+                                    <td class="px-4 py-2 border border-black">{{ $item->sold_at }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
 
         <div class="mt-4">
