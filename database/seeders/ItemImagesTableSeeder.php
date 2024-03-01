ace3557efc7626e50365f7a9c8e04a11d9bfe59c
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-03-02 08:17:32 +0900

    Added multiple initial images for products.

--- database/seeders/ItemImagesTableSeeder.php
+++ database/seeders/ItemImagesTableSeeder.php
@@ -11,134 +11,27 @@ class ItemImagesTableSeeder extends Seeder
 {
     public function run(): void
     {
-        $itemImages = [
-            [
-                'item_id' => 1,
-                'image_path' => 'item_images/dummy1.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
+        $dummyImages = [];
+        for ($i = 1; $i <= 15; $i++) {
+            $dummyImages[] = 'dummy' . $i . '.png';
+        }
 
-            [
-                'item_id' => 1,
-                'image_path' => 'item_images/dummy2.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
+        for ($itemId = 1; $itemId <= 15; $itemId++) {
+            $selectedImages = array_rand($dummyImages, 5);
+            foreach ($selectedImages as $index) {
+                $imagePath = 'item_images/' . $dummyImages[$index];
+                $itemImage = [
+                    'item_id' => $itemId,
+                    'image_path' => $imagePath,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ];
 
-            [
-                'item_id' => 1,
-                'image_path' => 'item_images/dummy3.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
+                $image = public_path('images/' . basename($imagePath));
+                Storage::put('public/' . $imagePath, file_get_contents($image));
 
-            [
-                'item_id' => 2,
-                'image_path' => 'item_images/dummy2.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-
-            [
-                'item_id' => 3,
-                'image_path' => 'item_images/dummy3.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-
-            [
-                'item_id' => 4,
-                'image_path' => 'item_images/dummy4.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-
-            [
-                'item_id' => 5,
-                'image_path' => 'item_images/dummy5.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-
-            [
-                'item_id' => 6,
-                'image_path' => 'item_images/dummy6.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-
-            [
-                'item_id' => 7,
-                'image_path' => 'item_images/dummy7.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-
-            [
-                'item_id' => 8,
-                'image_path' => 'item_images/dummy8.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-
-            [
-                'item_id' => 9,
-                'image_path' => 'item_images/dummy9.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-
-            [
-                'item_id' => 10,
-                'image_path' => 'item_images/dummy10.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-
-            [
-                'item_id' => 11,
-                'image_path' => 'item_images/dummy11.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-
-            [
-                'item_id' => 12,
-                'image_path' => 'item_images/dummy12.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-
-            [
-                'item_id' => 13,
-                'image_path' => 'item_images/dummy13.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-
-            [
-                'item_id' => 14,
-                'image_path' => 'item_images/dummy14.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-
-            [
-                'item_id' => 15,
-                'image_path' => 'item_images/dummy15.png',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-        ];
-
-        foreach ($itemImages as $itemImage) {
-
-            $imagePath = $itemImage['image_path'];
-            $image = public_path('images/' . basename($imagePath));
-            Storage::put('public/' . $imagePath, file_get_contents($image));
-
-            DB::table('item_images')->insert($itemImage);
+                DB::table('item_images')->insert($itemImage);
+            }
         }
     }
 }
